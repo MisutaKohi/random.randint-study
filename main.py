50a6b08dbfc8df4ceae4c850a53660b9d0ff7a4f
Ryan Morgan <R.Morgan5399@gmail.com>
2022-02-23 13:46:35 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+"""This script will represent a study of the random library's randint method. How random does it really generate
+an integer?
+
+This script will prompt a user to enter several attributes about the study they would like to execute. First, the
+script will prompt the user for a sample size, that is to say, how many random integers they want generated at a time.
+Next, how many times to repeat the test at that sample size. This will be roughly equal to the number of rows in
+the Excel file (less one for the column headers). Finally, the name of the Excel file where the user would like to
+export the findings of the study.
+
+Once the calculations finish, the program will open the designated Excel file and overwrite any pre-existing content.
+The file can then be opened in a dataframe for further analysis."""
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
